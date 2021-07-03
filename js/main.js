9f9e8f406fb7fb70c04094116e9d76f348d2d860
Libia Perez <alexandra220790@gmail.com>
2021-07-03 14:03:45 -0300

    agregando variables globales, el contructor y la clase del juego

--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ const naranja = document.getElementById("naranja");
 const verde = document.getElementById("verde");
 const button = document.getElementById("btnEmpezar");
 const ultimoNivel = 10;
+
+class Juego {
+  constructor() {
+    this.inicializar();
+  }
+
+  inicializar() {
+    button.classList.add("hide");
+  }
+}
+
+function empezarJuego() {
+  var juego = new Juego();
+}
